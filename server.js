b679d922dd7097ec214ae9e789d9c42171e3ca54
Ayoub Jabiri <ayoubjabiri.business@gmail.com>
2026-04-01 10:08:13 +0100

    Set up the user router and controller

--- server.js
+++ server.js
@@ -4,17 +4,18 @@ import express from "express";
 
 // Internal Modules
 import { dbConnect } from "./config/db.js";
+import userRoutes from "./routes/user.router.js";
 
 const app = express();
 
+// Database Connection
 config();
 const PORT = process.env.PORT;
-
 dbConnect();
 
-app.get("/", (req, res) => {
-    res.send("Hello");
-});
+app.use(express.json());
+
+app.use("/users", userRoutes);
 
 app.listen(PORT, () => {
     console.log(`The server is listening on port ${PORT}`);
